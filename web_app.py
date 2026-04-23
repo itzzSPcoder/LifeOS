a28de1a70f3861600176fe758d93092f95878d8d
Sparsh Bansal <SParsh003@users.noreply.huggingface.co>
2026-04-23 18:47:44 +0000

    Fix indentation 2Zero-indent version

--- web_app.py
+++ web_app.py
@@ -1,8 +1,4 @@
-import gradio as gr
-from lifeos.envs.student_week_openenv import LifeOSGymEnv
-env = LifeOSGymEnv()
-obs, info = env.reset()
-def run():
-      return str(obs)
-    demo = gr.Interface(fn=run, inputs=None, outputs="text")
-demo.launch()
+import gradio as gr; from lifeos.envs.student_week_openenv import LifeOSGymEnv
+env = LifeOSGymEnv(); obs, info = env.reset()
+def run(): return str(obs)
+demo = gr.Interface(fn=run, inputs=None, outputs="text"); demo.launch()
