051cdc2f08c02b304719844279f049aa92b003d4
Sparsh Bansal <SParsh003@users.noreply.huggingface.co>
2026-04-23 18:46:37 +0000

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,11 +1,8 @@
 import gradio as gr
 from lifeos.envs.student_week_openenv import LifeOSGymEnv
+env = LifeOSGymEnv()
+obs, info = env.reset()
 def run():
-      try:
-              env = LifeOSGymEnv()
-              obs, info = env.reset()
-              return str(obs)
-    except Exception as e:
-            return str(e)
-demo = gr.Interface(fn=run, inputs=None, outputs="text")
+      return str(obs)
+    demo = gr.Interface(fn=run, inputs=None, outputs="text")
 demo.launch()
